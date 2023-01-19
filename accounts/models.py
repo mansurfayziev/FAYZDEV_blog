44e75adc70eed4eaf4cba5a9d0e7ac46dc68e2e5
Mansur Fayziev <114176073+mansurfayziev@users.noreply.github.com>
2023-01-19 14:34:52 +0500

    Add files via upload

---
+++ accounts/models.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class CustomUser(AbstractUser):
+    age=models.PositiveIntegerField("Возраст",null=True,blank=True)
+    email=models.EmailField()
+ 
